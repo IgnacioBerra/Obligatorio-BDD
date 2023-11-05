62b80d6e7e490e7c39b4b36021e4ff0d0a531c98
IgnacioBerra <ignacio.berra@correo.ucu.edu.uy>
2023-11-05 19:06:34 -0300

    API + DOCKER

---
+++ API/Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly DataInfo _context;
+
+        public LoginController (DataInfo data)
+        {
+            _context = data;
+        }
+
+        [HttpGet("Logins")]
+        public IActionResult getLogins()
+        {
+            var loginDetails = _context.logins.AsQueryable();
+            return Ok(loginDetails);
+        }
+    }
+}
