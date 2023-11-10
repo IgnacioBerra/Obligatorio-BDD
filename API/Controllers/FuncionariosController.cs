81ad079eb409ec9bfd8fa043db7f380ae6882dea
IgnacioBerra <ignacio.berra@correo.ucu.edu.uy>
2023-11-10 11:39:12 -0300

    LC

--- API/Controllers/FuncionariosController.cs
+++ API/Controllers/FuncionariosController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using API.Data;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -7,5 +8,11 @@ namespace API.Controllers
     [ApiController]
     public class FuncionariosController : ControllerBase
     {
+        private readonly DataInfo _context;
+
+        public FuncionariosController(DataInfo data)
+        {
+            _context = data;
+        }
     }
 }
