67c4c454d87a4011a2dad992fe136ebffcf09677
IgnacioBerra <ignacio.berra@correo.ucu.edu.uy>
2023-11-14 10:21:08 -0300

    Controllers faltantes (faltan endpoints)

---
+++ API/Controllers/CarnetSaludController.cs
@@ -0,0 +1,19 @@
+﻿using API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarnetSaludController : ControllerBase
+    {
+
+        private readonly DataInfo _context;
+
+        public CarnetSaludController(DataInfo data)
+        {
+            _context = data;
+        }
+    }
+}
