408d2b6b013d90bab959f0dcb581d1b6ffedacd1
IgnacioBerra <ignacio.berra@correo.ucu.edu.uy>
2023-11-14 10:21:08 -0300

    Controllers faltantes (faltan endpoints)

---
+++ API/Controllers/AgendaController.cs
@@ -0,0 +1,18 @@
+﻿using API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AgendaController : ControllerBase
+    {
+        private readonly DataInfo _context;
+
+        public AgendaController(DataInfo data)
+        {
+            _context = data;
+        }
+    }
+}
---
+++ API/Controllers/PeriodosActualizacionController.cs
@@ -0,0 +1,41 @@
+﻿using API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeriodosActualizacionController : ControllerBase
+    {
+
+
+        private readonly DataInfo _context;
+
+        public PeriodosActualizacionController(DataInfo data)
+        {
+            _context = data;
+        }
+
+        [HttpPost("AddDate")]
+        public IActionResult addDate(DateTime fecha)
+        {
+
+
+
+            try
+            {
+                //var login = _context.periodosActualizacion.FromSqlRaw();
+
+                
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500);
+            }
+            return Ok();
+
+        }
+    }
+}
